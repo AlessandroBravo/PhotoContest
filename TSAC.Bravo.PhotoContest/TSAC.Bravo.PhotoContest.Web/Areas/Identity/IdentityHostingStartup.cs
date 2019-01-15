702f1f3e19766ae192c3ebbe740344ed5fb7fa78
Alessandro Bravo <alessandro.bravo@tecnicosuperiorekennedy.it>
2019-01-15 21:51:48 +0100

    added custom register page

---
+++ TSAC.Bravo.PhotoContest/TSAC.Bravo.PhotoContest.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TSAC.Bravo.PhotoContest.Web.Data;
+
+[assembly: HostingStartup(typeof(TSAC.Bravo.PhotoContest.Web.Areas.Identity.IdentityHostingStartup))]
+namespace TSAC.Bravo.PhotoContest.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
