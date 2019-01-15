ddc8118bedb925cbeeeda8ee5c9d95868946a2b6
Alessandro Bravo <alessandro.bravo@tecnicosuperiorekennedy.it>
2019-01-15 21:51:13 +0100

    added a ranking page

---
+++ TSAC.Bravo.PhotoContest/TSAC.Bravo.PhotoContest.Web/Pages/Ranking.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TSAC.Bravo.PhotoContest.Data;
+using TSAC.Bravo.PhotoContest.Data.Models;
+
+namespace TSAC.Bravo.PhotoContest.Web.Pages
+{
+    public class RankingModel : PageModel
+    {
+        private readonly IDataAccess _data;
+        public UserManager<IdentityUser> _userManager;
+        public IEnumerable<Photo> Photos { get; set; }
+       
+        /// <summary>
+        /// Costructor
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="userManager"></param>
+        public RankingModel(IDataAccess data, UserManager<IdentityUser> userManager)
+        {
+            _data = data;
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// OnGet function
+        /// </summary>
+        public void OnGet()
+        {
+            Photos = _data.GetRanking();
+        }
+    }
+}
